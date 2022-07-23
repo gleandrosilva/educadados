80bb7ac64f7274b3e3b965b57615cf0eab5097dc
Leandro Silva <leandrosilva@LMV-CONSULTORIA>
2022-07-23 19:41:53 -0300

    Modificação no script do download_files

--- src/download_files.py
+++ src/download_files.py
@@ -1,16 +1,16 @@
 import requests
+import os
 
 def download(url, directory):
 
-    FOLDER = '../data'
-    URL = 'https://download.inep.gov.br/microdados/microdados_censo_da_educacao_superior_2020.zip'
-
-    file = requests.get(URL)
+    file = requests.get(url)
     if file.status_code == requests.codes.OK:
-        with open(FOLDER, 'wb') as new_file:
+        with open(directory, 'wb') as new_file:
             new_file.write(file.content)
         print('Download do arquivo realizado com sucesso.')
     else:
         print(file.raise_for_status())
-
-download()        
+    
+if __name__ == "__main__":
+    URL = 'https://download.inep.gov.br/microdados/microdados_censo_da_educacao_superior_2020.zip'
+    FOLDER = '../data'
