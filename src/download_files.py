634d97e7320b7f1bef08e42e649b47b956abd414
Leandro Silva <leandrosilva@LMV-CONSULTORIA>
2022-07-28 11:50:53 -0300

    Atualização de inclusão do datetime e variaveis year

--- src/download_files.py
+++ src/download_files.py
@@ -1,3 +1,4 @@
+from datetime import date
 import requests
 import os
 
@@ -9,8 +10,17 @@ def download(url, directory):
             new_file.write(file.content)
         print('Download do arquivo realizado com sucesso.')
     else:
+        print('Este arquivo não existe')
         print(file.raise_for_status())
     
 if __name__ == "__main__":
-    URL = 'https://download.inep.gov.br/microdados/microdados_censo_da_educacao_superior_2020.zip'
+    URL = 'https://download.inep.gov.br/microdados/microdados_censo_da_educacao_superior_{}.zip'
     FOLDER = '../data'
+
+    last_year = date.today().year
+    first_year = last_year - 10
+
+    for year in range(first_year, last_year):
+        file_name = os.path.join(FOLDER, f'microdados_censo_da_educacao_superior_{year}.zip')
+        download(URL.format(year), file_name)
+      
