5cecb2a224b08164b54d83ae7a9147fdbf9232ab
Leandro Silva <leandrosilva@LMV-CONSULTORIA>
2022-07-28 19:45:18 -0300

    Modificação dos paremetros

--- src/download_files.py
+++ src/download_files.py
@@ -1,6 +1,7 @@
 from datetime import date
 import requests
 import os
+import json
 
 def download(url: str, directory: str) -> None:
     """_Função que faz o download de um arquivo_
@@ -17,16 +18,26 @@ def download(url: str, directory: str) -> None:
         print(f'Download do arquivo realizado com sucesso. Salvo em {directory}')
     else:
         print(f'[ERRO] URL {url} não existe')
-        print(file.raise_for_status())
+        #print(file.raise_for_status())
     
 if __name__ == "__main__":
-    URL = 'https://download.inep.gov.br/microdados/microdados_censo_da_educacao_superior_{}.zip'
-    FOLDER = '../data'
 
-    last_year = date.today().year
-    first_year = last_year - 10
+    # importar .json com lista de links
+    with open('../download_list.json') as json_file:
+        url_dict = json.load(json_file)['links']
 
-    for year in range(first_year, last_year):
-        file_name = os.path.join(FOLDER, f'microdados_censo_da_educacao_superior_{year}.zip')
-        download(URL.format(year), file_name)
+    for name, link in url_dict.items():
+        folder = '../data/' + name
+        url = link['url']
+        file_name = link['filename']
+        
+        last_year = date.today().year
+        first_year = last_year - 10
+
+        for year in range(first_year, last_year):
+            # criar a pasta caso ela não exista
+            os.mkdir(folder) if not os.path.exists(folder) else None
+
+            directory = os.path.join(folder, f'{file_name}_{year}.zip')
+            download(url.format(year), directory)
       
