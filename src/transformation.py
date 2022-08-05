b097b047765653c4703a567dce0d58293512a956
Leandro Silva <gleandrosilva@gmail.com>
2022-08-05 15:28:13 -0300

    Correção de dados

--- src/transformation.py
+++ src/transformation.py
@@ -17,7 +17,7 @@ df = pd.DataFrame()
 for file in files:
     with zip.ZipFile(folder + file, 'r') as zip_file:
         for name in zip_file.namelist():
-            if name.find('MICRODADOS_CADASTRO_CURSOS_{}') > 0:
+            if name.find('MICRODADOS_CADASTRO_CURSOS_2020') > 0:
                 with zip_file.open(name) as csv_file:
                     df_tmp = pd.read_csv(csv_file, encoding='latin1', sep=';')
                     df = df.append(df_tmp)
